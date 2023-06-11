be683928be9c4709b973435bd82e9d53e9b2513d
Alessandro <alessandrocatenacci22@gmail.com>
2023-06-11 21:02:39 +0200

    fixed bug

--- run_win.bat
+++ run_win.bat
@@ -8,17 +8,13 @@ set doc_dir=docs
 echo Compiling Java files...
 
 set javac_path=
-where /r "C:\Program Files" javac.exe >nul 2>&1 && (
-    set javac_path=javac.exe
-) || (
-    echo javac not found. Searching the entire file system. This may take a while depending on the file system size.
-    for /f "delims=" %%i in ('where /r \ javac.exe') do (
+for /f "delims=" %%i in ('where /r "C:\Program Files" javac.exe') do (
   set javac_path=%%i
-  goto :found_javac
-    )
-    ) || (
-        echo ERROR: javac not found.
-        exit /b 1
+) || (
+    echo javac not found in "C:\Program Files". Searching the entire file system. This may take a while depending on the file system size.
+    for /f "delims=" %%i in ('where /r \ javac.exe') do (
+        set javac_path=%%i
+        goto :found_javac
     )
 )
 :found_javac
@@ -43,13 +39,13 @@ xcopy /s /y "%src_dir%\resources\*" "%out_dir%\resources\"
 echo Generating documentation...
 
 set javadoc_path=
-where /r "C:\Program Files" javadoc.exe >nul 2>&1 && (
-    set javadoc_path=javadoc.exe
-) || (
-    echo javadoc not found. Searching the entire file system. This may take a while depending on the file system size.
-    for /f "delims=" %%i in ('where /r \ javadoc.exe') do (
+for /f "delims=" %%i in ('where /r "C:\Program Files" javadoc.exe') do (
   set javadoc_path=%%i
-  goto :found_javadoc
+) || (
+    echo javadoc not found in "C:\Program Files". Searching the entire file system. This may take a while depending on the file system size.
+    for /f "delims=" %%i in ('where /r \ javadoc.exe') do (
+        set javadoc_path=%%i
+        goto :found_javadoc
     )
 )
 :found_javadoc
@@ -63,17 +59,13 @@ if defined javadoc_path (
 echo Running Java application...
 
 set java_path=
-where /r "C:\Program Files" java.exe >nul 2>&1 && (
-    set java_path=java.exe
-) || (
-    echo java not found. Searching the entire file system. This may take a while depending on the file system size.
-    for /f "delims=" %%i in ('where /r \ java.exe') do (
+for /f "delims=" %%i in ('where /r "C:\Program Files" java.exe') do (
   set java_path=%%i
-  goto :found_java
-      )
-    ) || (
-        echo ERROR: java not found.
-        exit /b 1
+) || (
+    echo java not found in "C:\Program Files". Searching the entire file system. This may take a while depending on the file system size.
+    for /f "delims=" %%i in ('where /r \ java.exe') do (
+        set javac_path=%%i
+        goto :found_java
     )
 )
 :found_java
